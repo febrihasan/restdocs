3de242c62bbf82833ab77d1159c87772aa0c912a
febrihasan <febri.hasan@akarinti.tech>
2023-02-01 14:24:30 +0700

    build: application rest docs

---
+++ src/test/java/tech/febrihasan/demo/restdocs/RestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.febrihasan.demo.restdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
